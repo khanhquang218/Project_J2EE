817cb71812d50a6408da90482338165553a72ccb
toiQS <88361704+toiQS@users.noreply.github.com>
2023-11-28 20:47:48 +0700

    Create UserModel.java

---
+++ src/main/webapp/models/UserModel.java
@@ -0,0 +1,17 @@
+package models;
+
+import java.time.LocalDate;
+
+public class UserModel {
+    public String UserID;
+    public String FirstName;
+    public String LastName;
+    public String Email;
+    public String Phone;
+    public String UserAccount;
+    public LocalDate DayOfBirth;
+    public String Gender;
+    public String Address;
+    public String Images;
+
+}
