5a2de1648a043cb35cd80e704e7b6c544c9d8234
QuanNguyen1946 <96603009+QuanNguyen1946@users.noreply.github.com>
2023-11-29 10:46:22 +0700

    update

--- src/main/webapp/controllers/PostController.java
+++ src/main/webapp/controllers/PostController.java
@@ -9,18 +9,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet({"/"})
 public class PostController extends HttpServlet {
     private final PostServices postServices = new PostServices();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        super.doGet(req, resp);
-//        List<PostModel> postModelList = postServices.GetAllPosts();
-//        req.setAttribute("postList", postModelList);
-//        req.getRequestDispatcher("/view/home.jsp");
-        PostModel postModel = postServices.FecthPostModel("1");
-        req.setAttribute("postList", postModel);
+        List<PostModel> postModelList = postServices.GetAllPosts();
+        req.setAttribute("postList", postModelList);
         req.getRequestDispatcher("/view/home.jsp");
+//        Test GetAllPosts
+//        PostModel postModel = postServices.FecthPostModel("1");
+//        req.setAttribute("postList", postModel);
+//        req.getRequestDispatcher("/view/home.jsp");
+//        Test FecthPostModel
+
     }
 }
