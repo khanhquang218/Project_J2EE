2bb641b5b2d366783e9a113bdd23f679d0cd8d50
QuanNguyen1946 <96603009+QuanNguyen1946@users.noreply.github.com>
2023-11-29 23:17:00 +0700

    update CommentModel

--- src/main/webapp/models/CommentModel.java
+++ src/main/webapp/models/CommentModel.java
@@ -1,4 +1,68 @@
 package models;
 
+import java.time.LocalDate;
+
 public class CommentModel {
+    public String CommentID;
+    public String Content;
+    public String PostID;
+    public String ReplyTo;
+    public String UserID;
+    public LocalDate CreateDate;
+    public CommentModel(String CommentID, String Content, String PostID, String ReplyTo,String UserID, LocalDate CreateDate){
+        this.CommentID = commentID;
+        this.Content = content;
+        this.PostID = postID;
+        this.ReplyTo = replyTo
+        this.UserID = userID;
+        this.CreateDate = createDate;
+    }
+
+    public String getCommentID() {
+        return CommentID;
+    }
+
+    public void setCommentID(String CommentID) {
+        this.CommentID = CommentID;
+    }
+
+    public String getContent() {
+        return Content;
+    }
+
+    public void setContent(String Content) {
+        this.Content = Content;
+    }
+
+    public String getPostID() {
+        return PostID;
+    }
+
+    public void setPostID(String PostID) {
+        this.PostID = PostID;
+    }
+
+    public String getReplyTo() {
+        return ReplyTo;
+    }
+
+    public void setReplyTo(String ReplyTo) {
+        this.ReplyTo = ReplyTo;
+    }
+
+    public String getUserID() {
+        return UserID;
+    }
+
+    public void setUserID(String UserID) {
+        this.UserID = UserID;
+    }
+
+    public LocalDate CreateDate() {
+        return CreateDate;
+    }
+
+    public void CreateDate(LocalDate CreateDate) {
+        this.CreateDate = CreateDate;
+    }
 }
