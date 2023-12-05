2a87392a5fae755aee41a961100dd58b6c374a39
toiQS <88361704+toiQS@users.noreply.github.com>
2023-12-05 16:51:03 +0700

    Update InteractServices.java

--- src/main/webapp/services/InteractServices.java
+++ src/main/webapp/services/InteractServices.java
@@ -23,8 +23,7 @@ public class InteractServices {
                 InteractModel interactModel = new InteractModel(
                         resultSet.getString("InteractID"),
                         resultSet.getInt("ParemtID"),
-                        resultSet.getString("UserID"),
-                        resultSet.getInt("Count")
+                        resultSet.getString("UserID")
                 );
                 interactModelList.add(interactModel);
             }
@@ -33,6 +32,33 @@ public class InteractServices {
             exception.printStackTrace();
         }
         return interactModelList;
+    }
+    public List<InteractModel> GetAllInteractOfPost(int PostID){
+        PostServices postServices = new PostServices();
+        var resultOfCheckPost = postServices.CheckPostIsExists(PostID);
+        List<InteractModel> interactModelList = new ArrayList<>();
+        String query = String.format("select * from interact where PostID = %s",PostID);
+        if(resultOfCheckPost == true){
+            try {
+                Configura.CheckDrive();
+                Connection connection = DriverManager.getConnection(configura.JDBC_URL, configura.JDBC_USER, configura.JDBC_PASSWORD);
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(query);
+                while (resultSet.next()){
+                    InteractModel interactModel = new InteractModel(
+                            resultSet.getString("InteractID"),
+                            resultSet.getInt("ParemtID"),
+                            resultSet.getString("UserID")
+                    );
+                    interactModelList.add(interactModel);
+                }
+            }
+            catch (SQLException exception){
+                exception.printStackTrace();
+            }
+            return interactModelList;
+        }
+        return null;
     }
     public boolean CreateAInteract(InteractModel newInteractModel){
         PostServices postServices = new PostServices();
@@ -48,7 +74,6 @@ public class InteractServices {
                 preparedStatement.setString(1,newInteractModel.InteractID);
                 preparedStatement.setString(2,String.valueOf(newInteractModel.ParentID));
                 preparedStatement.setString(3, newInteractModel.UserID);
-                preparedStatement.setString(4,String.valueOf(newInteractModel.Count));
                 preparedStatement.executeUpdate();
                 return true;
             }
