ad9987a39c2ded7cad9dbcda34a04a2186035819
toiQS <88361704+toiQS@users.noreply.github.com>
2023-12-06 04:15:21 +0700

    Update FetchListPostOfUser.java

--- src/main/webapp/controllers/FetchListPostOfUser.java
+++ src/main/webapp/controllers/FetchListPostOfUser.java
@@ -3,17 +3,21 @@ package controllers;
 import services.PostServices;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+@WebServlet({"/posts/5"})
 public class FetchListPostOfUser extends HttpServlet {
     private PostServices postServices = new PostServices();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //        super.doGet(req, resp);
-        var resultOfListPostOfUser = postServices.
+        var resultOfListPostOfUser = postServices.GetPostOfUserID("5");
+        System.out.printf(resultOfListPostOfUser.toString());
+        req.setAttribute("resultOfListPostOfUser", resultOfListPostOfUser);
+        req.getRequestDispatcher("/view/home.jsp");
     }
 }
