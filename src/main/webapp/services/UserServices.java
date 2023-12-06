9c7c8e2389956d11180b6056573f9bbad93a2fdf
toiQS <88361704+toiQS@users.noreply.github.com>
2023-12-06 21:54:49 +0700

    Update UserServices.java

--- src/main/webapp/services/UserServices.java
+++ src/main/webapp/services/UserServices.java
@@ -10,9 +10,9 @@ import java.util.List;
 * kiểm tra việc bổ sung drive
 * lấy toàn bộ thông tin người dùng trong database
 * tìm kiếm người dùng bằng UserID người dùng
-* tìm kiếm người dùng bằng UserName của người dùng
+* tìm kiếm người dùng bằng UserAccount của người dùng
 * kiểm tra mã số người dùng đó có tồn tại hay chưa thông quá userID
-* kiểm tra tên người dùng có tồn tại hay không thông qua UserName
+* kiểm tra tên người dùng có tồn tại hay không thông qua UserAccount
 */
 public class UserServices {
     private static final Configura configura = new Configura();
@@ -79,38 +79,41 @@ public class UserServices {
         return  null;
     }
 
-    //tìm kiếm user bằng UserName
-    public  List<UserModel> FetchUserModelByUserName(String UserName){
+    //tìm kiếm user bằng UserAccount
+    public  List<UserModel> FetchUserModelByUserName(String UserAccount){
         List<UserModel> userModelList = new ArrayList<>();
-        String query = String.format("select * from person where UserName = %s", UserName);
-        var resultOfCheckUserNameExists = CheckUserNameExists(UserName);
-        try {
-            Configura.CheckDrive();
-            Connection connection = DriverManager.getConnection(configura.JDBC_URL, configura.JDBC_USER, configura.JDBC_PASSWORD);
-            Statement statement =connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(query);
-            while (resultSet.next()){
-                UserModel userModel = new UserModel(
-                        resultSet.getString("UserID"),
-                        resultSet.getString("FirstName"),
-                        resultSet.getString("LastName"),
-                        resultSet.getString("Email"),
-                        resultSet.getString("Phone"),
-                        resultSet.getString("UserAccount"),
-                        resultSet.getDate("DayOfBirth").toLocalDate(),
-                        resultSet.getString("Gender"),
-                        resultSet.getString("Address")
-                );
-                userModelList.add(userModel);
+        String query = String.format("select * from person where UserAccount = %s", UserAccount);
+        var resultOfCheckUserNameExists = CheckUserNameExists(UserAccount);
+        if(resultOfCheckUserNameExists){
+            try {
+                Configura.CheckDrive();
+                Connection connection = DriverManager.getConnection(configura.JDBC_URL, configura.JDBC_USER, configura.JDBC_PASSWORD);
+                Statement statement =connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(query);
+                while (resultSet.next()){
+                    UserModel userModel = new UserModel(
+                            resultSet.getString("UserID"),
+                            resultSet.getString("FirstName"),
+                            resultSet.getString("LastName"),
+                            resultSet.getString("Email"),
+                            resultSet.getString("Phone"),
+                            resultSet.getString("UserAccount"),
+                            resultSet.getDate("DayOfBirth").toLocalDate(),
+                            resultSet.getString("Gender"),
+                            resultSet.getString("Address")
+                    );
+                    userModelList.add(userModel);
+                }
+            }catch (SQLException exception){
+                exception.printStackTrace();
             }
-        }catch (SQLException exception){
-            exception.printStackTrace();
+            return  userModelList;
         }
-        return  userModelList;
+        return  null;
     }
-    //Kiểm tra tên người dùng có tồn tại hay không thông UserName
-    public boolean CheckUserNameExists(String UserName){
-        String query = String.format("select  * from person where UserName = %s ",UserName);
+    //Kiểm tra tên người dùng có tồn tại hay không thông UserAccount
+    public boolean CheckUserNameExists(String UserAccount){
+        String query = String.format("select * from person where UserAccount = %s ",UserAccount);
         try{
             Configura.CheckDrive();
             Connection connection = DriverManager.getConnection(configura.JDBC_URL, configura.JDBC_USER, configura.JDBC_PASSWORD);
@@ -119,6 +122,7 @@ public class UserServices {
             if(resultSet != null){
                 return true;
             }
+            connection.close();
         }
         catch (SQLException exception){
             exception.printStackTrace();
