9fd9299319e0a117d1f16fca82d5195cf8efb0fb
toiQS <88361704+toiQS@users.noreply.github.com>
2023-12-06 22:50:56 +0700

    Update FecthListPostOfUserByUserName.java

--- src/main/webapp/controllers/FecthListPostOfUserByUserName.java
+++ src/main/webapp/controllers/FecthListPostOfUserByUserName.java
@@ -15,7 +15,7 @@ public class FecthListPostOfUserByUserName extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //        super.doGet(req, resp);
-        var resultOfListPostOfUserByUserName = postServices.GetPostsByUserName("a");
+        var resultOfListPostOfUserByUserName = postServices.GetPostsByUserName("aaaa");
         System.out.println(resultOfListPostOfUserByUserName);
         req.setAttribute("resultOfPostOfUserByUserName",resultOfListPostOfUserByUserName);
         req.getRequestDispatcher("/view/home.jsp");
