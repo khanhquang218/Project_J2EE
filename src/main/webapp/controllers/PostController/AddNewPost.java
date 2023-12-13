6273b5443f112d2ddeb0187113cfaf620147ed8a
toiQS <88361704+toiQS@users.noreply.github.com>
2023-12-13 15:17:19 +0700

    Create AddNewPost.java

---
+++ src/main/webapp/controllers/PostController/AddNewPost.java
@@ -0,0 +1,17 @@
+package controllers.PostController;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "add-new-post",urlPatterns = {"/posts/addNewPost/"})
+public class AddNewPost extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        req.getRequestDispatcher("/view/CreatePost.jsp").forward(req,resp);
+    }
+}
