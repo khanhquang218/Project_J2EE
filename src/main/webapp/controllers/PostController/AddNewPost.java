56bc2751a972e98022115862c9fa8bfd960507e0
toiQS <88361704+toiQS@users.noreply.github.com>
2023-12-14 10:12:31 +0700

    Update AddNewPost.java

--- src/main/webapp/controllers/PostController/AddNewPost.java
+++ src/main/webapp/controllers/PostController/AddNewPost.java
@@ -11,33 +11,36 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDate;
 
-@WebServlet(name = "add-new-post",urlPatterns = {"/posts/addNewPost/"})
+@WebServlet({"/user/newPost/","/user/newPost/add"})
 public class AddNewPost extends HttpServlet {
     private PostServices postServices = new PostServices();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //        super.doGet(req, resp);
-        req.getRequestDispatcher("/view/CreatePost.jsp").forward(req,resp);
+        String url = req.getRequestURI();
+        if (url.contains("add")){
+            processResponse(req,resp);
+        }
+        else req.getRequestDispatcher("/view/CreatePost.jsp").forward(req,resp);
     }
     private int generatePostID() {
         // Implement a more robust method for generating unique post IDs.
         return postServices.GetAllPosts().size() + 1;
     }
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void processResponse(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //        super.doPost(req, resp);
         var postID = generatePostID();
         String content = req.getParameter("text-post");
         LocalDate modifed = LocalDate.now();
         LocalDate lastModifedTime = LocalDate.now();
         LocalDate datePost = LocalDate.now();
-        PostModel postModel = new PostModel(postID,"1","null",content,datePost,
-                modifed,lastModifedTime);
+        PostModel postModel = new PostModel(postID, "1", "null", content, datePost,
+                modifed, lastModifedTime);
         try {
             postServices.CreateNewPost(postModel);
-            resp.sendRedirect(req.getContextPath() +"/posts/");
-        }
-        catch (Exception exception){
+            req.setAttribute("successMessage", "Post created successfully!");
+            req.getRequestDispatcher("/view/CreatePost.jsp").forward(req, resp);
+        } catch (Exception exception) {
             exception.printStackTrace();
         }
     }
