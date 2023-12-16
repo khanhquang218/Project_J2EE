ce9715e2d17424d8057af1fdc7345b67b56dba0f
toiQS <88361704+toiQS@users.noreply.github.com>
2023-12-16 15:26:30 +0700

    Update ShowViewIndex.java

--- src/main/webapp/controllers/ShowViewIndex.java
+++ src/main/webapp/controllers/ShowViewIndex.java
@@ -12,6 +12,6 @@ public class ShowViewIndex extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //        super.doGet(req, resp);
-        req.getRequestDispatcher("index.jsp").forward(req,resp);
+        req.getRequestDispatcher("/index.jsp").forward(req,resp);
     }
 }
