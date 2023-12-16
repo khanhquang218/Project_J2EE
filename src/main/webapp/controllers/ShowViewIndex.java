5aa2f33651c24c5e6fe9cdc6b1dd9af51c6ae880
toiQS <88361704+toiQS@users.noreply.github.com>
2023-12-16 15:24:34 +0700

    Create ShowViewIndex.java

---
+++ src/main/webapp/controllers/ShowViewIndex.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet({"/posts/"})
+public class ShowViewIndex extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        req.getRequestDispatcher("index.jsp").forward(req,resp);
+    }
+}
